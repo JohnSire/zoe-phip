6151e5b81b4c91785e416cf9fcf53867827dc19c
zjy <652105072@qq.com>
2016-03-12 17:51:52 +0800

    集成dubbox

---
+++ zoe-phip/4.client/phip-client-host/src/main/java/com/zoe/phip/client/host/Main.java
@@ -0,0 +1,16 @@
+package com.zoe.phip.client.host;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by zengjiyang on 2016/1/22.
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext(new String[] {"application-context-provider.xml", "spring-mybatis.xml"});
+        context.start();
+        System.out.println("按任意键退出");
+        System.in.read();
+    }
+}
