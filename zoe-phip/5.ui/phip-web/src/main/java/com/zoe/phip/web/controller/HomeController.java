29daf180300ec934e80ed8f54e445dae018b7b52
zjy <652105072@qq.com>
2016-03-14 10:34:00 +0800

    增加前端测试界面

--- zoe-phip/5.ui/phip-web/src/main/java/com/zoe/phip/web/controller/HomeController.java
+++ zoe-phip/5.ui/phip-web/src/main/java/com/zoe/phip/web/controller/HomeController.java
@@ -1,7 +1,24 @@
 package com.zoe.phip.web.controller;
 
+import com.zoe.phip.model.base.ServiceResult;
+import com.zoe.phip.service.in.demo.DeptService;
+import com.zoe.phip.web.bean.BeanFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * Created by zengjiyang on 2016/3/11.
  */
+@Controller
+@RequestMapping("/home")
 public class HomeController {
+    @RequestMapping("/test")
+    public String toIndex(HttpServletRequest request, Model model){
+        DeptService deptService = (DeptService) BeanFactory.getBean("DeptService");
+        ServiceResult serviceResult= deptService.deleteById("d4b87a02a7f349a3b5afd2758ad3d294");
+        return "test";
+    }
 }
