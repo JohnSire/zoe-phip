d48c74ab7479511a6701c939c7759d816fe95c8d
zjy <652105072@qq.com>
2016-03-14 16:50:20 +0800

    增加代码生成工具

---
+++ zoe-phip/6.tools/zoe-phip-code-generator/template/dto/${className}Dto.java
@@ -0,0 +1,28 @@
+<#include "/java_copyright.include">
+<#assign className = table.className>
+<#assign classNameLower = className?uncap_first>
+package ${basepackage}.dto;
+
+/**
+ * @author
+ * @date ${now?string("yyyy-MM-dd")}
+ * @version 1.0
+ */
+public class ${className} extends CoreModel {
+<#list table.columns as field>
+    /** ${field.remarks} */
+    private ${field.javaType} ${field.entityName?uncap_first};
+
+</#list>
+
+<#list table.columns as field>
+    public ${field.javaType} get${field.entityName?cap_first}(){
+        return  this.${field.entityName?uncap_first};
+        }
+
+    public void set${field.entityName?cap_first}(${field.javaType} ${field.entityName?uncap_first}){
+        this.${field.entityName?uncap_first} = ${field.entityName?uncap_first};
+        }
+
+</#list>
+}
