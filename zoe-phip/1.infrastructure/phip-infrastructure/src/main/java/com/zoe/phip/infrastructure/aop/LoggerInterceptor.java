2ab7ab505b466817457fb24682284293ed45a7f5
zwb <zhangwenbin@outlook.com>
2016-03-17 17:07:21 +0800

    AOP/LOGGER

--- zoe-phip/1.infrastructure/phip-infrastructure/src/main/java/com/zoe/phip/infrastructure/aop/LoggerInterceptor.java
+++ zoe-phip/1.infrastructure/phip-infrastructure/src/main/java/com/zoe/phip/infrastructure/aop/LoggerInterceptor.java
@@ -2,6 +2,7 @@ package com.zoe.phip.infrastructure.aop;
 
 import com.zoe.phip.infrastructure.util.StringUtil;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,15 +12,30 @@ import java.lang.reflect.Method;
  * Created by 文彬 on 2016/3/16.
  */
 public class LoggerInterceptor {
+
+
     private Logger logger = LoggerFactory.getLogger(LoggerInterceptor.class);
 
-    public void doAround(JoinPoint point) throws Throwable {
+    public void doBefore(JoinPoint point) {
+        // TODO: 2016/3/17 相关权限验证
+
+    }
+
+    public void doAfter(JoinPoint joinPoint) {
+        // TODO: 2016/3/17
+    }
+
+    public void doAfterReturn(JoinPoint joinPoint, String result) {
+        // TODO: 2016/3/17
+    }
+
+    public Object doAround(ProceedingJoinPoint joinPoint) throws Throwable {
         //参数
-        Object[] params = point.getArgs();
-        String methodName = point.getSignature().getName();
+        Object[] params = joinPoint.getArgs();
+        String methodName = joinPoint.getSignature().getName();
         if (!StringUtil.isNullOrWhiteSpace(methodName)) {
             if (!(methodName.startsWith("set") || methodName.startsWith("get") || methodName.startsWith("query"))) {
-                Class clazz = point.getTarget().getClass();
+                Class clazz = joinPoint.getTarget().getClass();
                 Method method = clazz.getMethod(methodName);
                 if (method != null) {
                     boolean hasAnnotation = method.isAnnotationPresent(Action.class);
@@ -35,5 +51,12 @@ public class LoggerInterceptor {
                 }
             }
         }
+        return joinPoint.proceed();
+    }
+
+    public void doAfterThrowing(JoinPoint joinPoint, Throwable ex) {
+        // TODO: 2016/3/17 异常处理
+        if (logger.isErrorEnabled())
+            logger.error("异常处理消息", ex);
     }
 }
