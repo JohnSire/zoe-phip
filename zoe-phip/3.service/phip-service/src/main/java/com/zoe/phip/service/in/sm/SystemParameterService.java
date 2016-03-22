c2ef8edef1d3ec946ab9186e83843517e4c43900
zhanghao <zhanghaofir@sina.com>
2016-03-22 11:34:06 +0800

    系统参数模块

---
+++ zoe-phip/3.service/phip-service/src/main/java/com/zoe/phip/service/in/sm/SystemParameterService.java
@@ -0,0 +1,20 @@
+/*
+ * Powered By zoe
+ * Since 2008 - 2016
+ */
+
+
+package com.zoe.phip.service.in.sm;
+
+import com.zoe.phip.model.sm.SystemParameter;
+import com.zoe.phip.service.in.BaseInService;
+
+/**
+ * 对外发布的服务接口
+ * @author
+ * @date 2016-03-22
+ * @version 1.0
+ */
+public interface SystemParameterService extends BaseInService<SystemParameter>  {
+
+}
