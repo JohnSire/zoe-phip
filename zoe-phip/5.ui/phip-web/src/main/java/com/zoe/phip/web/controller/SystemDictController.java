06358cba9f812ff6a335a1c6f26af4bec8e0e875
zxc <456>
2016-03-23 17:46:11 +0800

    菜单管理

--- zoe-phip/5.ui/phip-web/src/main/java/com/zoe/phip/web/controller/SystemDictController.java
+++ zoe-phip/5.ui/phip-web/src/main/java/com/zoe/phip/web/controller/SystemDictController.java
@@ -1,9 +1,18 @@
 package com.zoe.phip.web.controller;
 
 
+import com.sun.org.apache.bcel.internal.generic.RETURN;
+import com.zoe.phip.infrastructure.entity.PageList;
+import com.zoe.phip.infrastructure.entity.QueryPage;
+import com.zoe.phip.infrastructure.entity.ServiceResultT;
+import com.zoe.phip.model.sm.SystemUser;
+import com.zoe.phip.service.in.sm.SystemUserService;
+import com.zoe.phip.web.bean.BeanFactory;
+import com.zoe.phip.web.bean.Constant;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -29,5 +38,14 @@ public class SystemDictController {
     @RequestMapping("/category")
     public String ToDictCategoryDetail(HttpServletRequest request, Model model) {
         return "/dict/category";
+    }
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public ServiceResultT<PageList<SystemUser>> getDict(HttpServletRequest request, Model model){
+        SystemUserService systemUserService = BeanFactory.getBean(Constant.SYSTEM_USER_SERVICE);
+        QueryPage page=new QueryPage(1,30);
+        ServiceResultT<PageList<SystemUser>> resultT= systemUserService.getList(page, SystemUser.class);
+        return resultT;
     }
 }
