80d10c90dc3b0e06ee82e00fdf80614c6f15ae61
zjy <652105072@qq.com>
2016-03-25 15:22:01 +0800

    增加控制层的单元测试

--- zoe-phip/5.ui/phip-web/src/test/java/com/zoe/phip/web/controller/SystemUserControllerTest.java
+++ zoe-phip/5.ui/phip-web/src/test/java/com/zoe/phip/web/controller/SystemUserControllerTest.java
@@ -1,44 +1,51 @@
 package com.zoe.phip.web.controller;
 
+import com.zoe.phip.infrastructure.entity.ServiceResult;
+import com.zoe.phip.infrastructure.entity.ServiceResultT;
+import com.zoe.phip.model.sm.SystemUser;
+import com.zoe.phip.web.context.DataContext;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.awt.*;
 
 import static org.junit.Assert.*;
 
 /**
  * Created by zengjiyang on 2016/3/25.
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({"classpath*:spring-mvc.xml","classpath*:application-context-consumer.xml"})
-public class SystemUserControllerTest {
-
-    private MockHttpServletRequest request;
-    private MockHttpServletResponse response;
+public class SystemUserControllerTest extends BaseControllerTest {
 
     @Autowired
     private SystemUserController systemUserController;
 
-    @Before
-    public void setUp(){
-        request = new MockHttpServletRequest();
-        request.setCharacterEncoding("UTF-8");
-        response = new MockHttpServletResponse();
-    }
-
     @Test
     public void testGetUserInfo() throws Exception {
         try {
-            request.setParameter("userName", "admin");
-            request.setParameter("password", "2");
-            assertEquals("login",systemUserController.getUserInfo(request,null)) ;
+            ServiceResultT<SystemUser> result= systemUserController.getUserInfo(request,null);
+            System.out.println(result.getResult().getName());
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+
+    @Test
+    public void userAddTest(){
+        SystemUser user=new SystemUser();
+        user.setState(1);
+        user.setName("test");
+        user.setPassword("1");
+        user.setLoginName("test");
+        ServiceResult result=systemUserController.addUserInfo(user);
+        Assert.assertTrue(result.getIsSuccess());
     }
 }
