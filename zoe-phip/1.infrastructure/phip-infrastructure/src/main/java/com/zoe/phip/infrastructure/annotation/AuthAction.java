e1acdae8ff79492a8bb6c3fc5cf6cff4a068e606
zjy <652105072@qq.com>
2016-03-31 10:54:01 +0800

    新增权限相关注解

--- zoe-phip/1.infrastructure/phip-infrastructure/src/main/java/com/zoe/phip/infrastructure/annotation/AuthAction.java
+++ zoe-phip/1.infrastructure/phip-infrastructure/src/main/java/com/zoe/phip/infrastructure/annotation/AuthAction.java
@@ -1,17 +1,16 @@
 package com.zoe.phip.infrastructure.annotation;
 
-import com.zoe.phip.infrastructure.security.MenuCode;
 import com.zoe.phip.infrastructure.security.Permission;
 
 import java.lang.annotation.*;
 
 /**
- * Created by zengjiyang on 2016/3/29.
+ * Created by zengjiyang on 2016/3/31.
  */
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-public @interface ZoeRole {
+public @interface AuthAction {
     /**
      * 权限
      * @return
@@ -19,9 +18,8 @@ public @interface ZoeRole {
     Permission[] permission();
 
     /**
-     * 菜单编码
+     * 中文名称
      * @return
      */
-    MenuCode[] code();
-
+    String name();
 }
---
+++ zoe-phip/1.infrastructure/phip-infrastructure/src/main/java/com/zoe/phip/infrastructure/annotation/AuthControllers.java
@@ -0,0 +1,19 @@
+package com.zoe.phip.infrastructure.annotation;
+
+import com.zoe.phip.infrastructure.security.MenuCode;
+
+import java.lang.annotation.*;
+
+/**
+ * Created by zengjiyang on 2016/3/31.
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface AuthControllers {
+    /**
+     * 菜单编码
+     * @return
+     */
+    AuthController[] value();
+}
