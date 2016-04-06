c5001956c795637d3661b6053907ffbc8fb0f6f5
zjy <652105072@qq.com>
2016-04-06 17:17:52 +0800

    增加register,repository项目

---
+++ zoe-phip/2.module/phip-register/phip-register-service-impl/src/main/java/com/zoe/phip/register/service/impl/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * 注册服务实现
+ * Created by zengjiyang on 2016/4/6.
+ */
+package com.zoe.phip.register.service.impl;
---
+++ zoe-phip/2.module/phip-register/phip-register-service/src/main/java/com/zoe/phip/register/service/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * 注册服务
+ * Created by zengjiyang on 2016/4/6.
+ */
+package com.zoe.phip.register.service;
