64bddb43e205ee93a8d4d64023a05dfa1f251621
Eason <zhangwenbin@outlook.com>
2016-04-19 14:24:03 +0800

    RegisterUtil

---
+++ zoe-phip/2.module/phip-register/phip-register-service-impl/src/main/java/com/zoe/phip/register/util/RegisterType.java
@@ -0,0 +1,37 @@
+package com.zoe.phip.register.util;
+
+
+/**
+ * Created by zhangwenbin on 2016/4/19.
+ */
+public final class RegisterType {
+
+    public static final String PATIENT_ADD_SUCUESS = "template/patient/out/个人信息注册服务响应信息-正向.tbl";
+    public static final String PATIENT_ADD_ERROR = "template/patient/out/个人信息注册服务响应信息-反向.tbl";
+
+    public static final String PATIENT_UPDATE_SUCUESS = "template/patient/out/个人信息更新服务响应信息-正向.tbl";
+    public static final String PATIENT_UPDATE_ERROR = "template/patient/out/个人信息更新服务响应信息-反向.tbl";
+
+    public static final String PATIENT_UNION_SUCUESS = "template/patient/out/个人信息合并服务响应信息-正向.tbl";
+    public static final String PATIENT_UNION_ERROR = "template/patient/out/个人信息合并服务响应信息-反向.tbl";
+
+    public static final String PATIENT_QUERY_SUCUESS = "template/patient/out/个人信息查询服务响应信息-正向.tbl";
+    public static final String PATIENT_QUERY_ERROR = "template/patient/out/个人信息查询服务响应信息-反向.tbl";
+
+
+    public static final String DOCTOR_SUCUESS = "template/staff/out/医护人员信息服务响应成功.tbl";
+    public static final String DOCTOR_ERROR = "template/staff/out/医护人员信息服务响应异常.tbl";
+
+
+    public static final String DOCTOR_QUERY_SUCUESS = "template/staff/out/医护人员信息查询服务响应信息-正向.tbl";
+    public static final String DOCTOR_QUERY_ERROR = "template/staff/out/医护人员信息查询服务响应信息-反向.tbl";
+
+    public static final String ORG_ADD_SUCCESS = "";
+    public static final String ORG_ADD_ERROR = "";
+
+    public static final String ORG_UPDATE_SUCCESS = "";
+    public static final String ORG_UPDATE_ERROR = "";
+
+    public static final String ORG_QUERY_SUCCESS = "";
+    public static final String ORG_QUERY_ERROR = "";
+}
