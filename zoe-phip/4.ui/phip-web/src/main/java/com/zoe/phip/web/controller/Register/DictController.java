b60060b325f7655aaa97c3c98ed116dfd3c0172f
qinghuangl <814400623@qq.com>
2016-04-21 10:36:05 +0800

    新增注册类界面及控制器

---
+++ zoe-phip/4.ui/phip-web/src/main/java/com/zoe/phip/web/controller/Register/DictController.java
@@ -0,0 +1,7 @@
+package com.zoe.phip.web.controller.Register;
+
+/**
+ * Created by linqinghuang on 2016/4/21.
+ */
+public class DictController {
+}
---
+++ zoe-phip/4.ui/phip-web/src/main/java/com/zoe/phip/web/controller/Register/OrganizationController.java
@@ -0,0 +1,16 @@
+package com.zoe.phip.web.controller.Register;
+
+/**
+ * Created by linqinghuang on 2016/4/21.
+ */
+public class OrganizationController {
+    //region 医疗机构
+        public String ToMedicalOrgList
+    //endregion
+
+    //region 卫生管理机构
+    //endregion
+
+    //region 第三方机构管理
+    //endregion
+}
