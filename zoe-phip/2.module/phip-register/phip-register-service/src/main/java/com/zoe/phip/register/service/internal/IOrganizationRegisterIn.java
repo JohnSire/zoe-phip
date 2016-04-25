1a11dcadf2ec6dfb728d3258ece2f9b4782e37dc
hyf <510287342@qq.com>
2016-04-25 11:45:39 +0800

    医疗机构管理代码

--- zoe-phip/2.module/phip-register/phip-register-service/src/main/java/com/zoe/phip/register/service/internal/IOrganizationRegisterIn.java
+++ zoe-phip/2.module/phip-register/phip-register-service/src/main/java/com/zoe/phip/register/service/internal/IOrganizationRegisterIn.java
@@ -1,8 +1,11 @@
 package com.zoe.phip.register.service.internal;
 
+import com.zoe.phip.infrastructure.entity.PageList;
+import com.zoe.phip.infrastructure.entity.QueryPage;
 import com.zoe.phip.infrastructure.entity.ServiceResultT;
 import com.zoe.phip.register.model.OrgDeptInfo;
 
+import java.util.List;
 import java.util.Map;
 
 /**医疗机构注册服务
@@ -30,4 +33,30 @@ public interface IOrganizationRegisterIn {
      * @return
      */
     ServiceResultT<OrgDeptInfo> organizationDetailQuery(Map<String,Object> map);
+
+    /**
+     * 1)医疗卫生机构（科室）信息删除
+     * @param id
+     * @return
+     */
+    boolean organizationDelete(String id);
+
+    /**
+     * 查询医疗机构（科室）类别字典列表，用于构造树
+     * @param deptParentCode
+     * @return
+     */
+    List<OrgDeptInfo> dictItemListQuery(String deptParentCode);
+
+    /**
+     * 查询医疗机构（科室）列表，内部使用
+     * @param deptParentCode
+     * @param key
+     * @param page
+     * @return
+     */
+    PageList<OrgDeptInfo> organizationListQuery(String deptParentCode, String key, QueryPage page);
+
+
+    
 }
