14f8c724cfbb8b06f24dc6b90d1ad36bf495b0ed
zxc <456>
2016-04-27 11:05:05 +0800

    个人信息管理接口对接

--- zoe-phip/4.ui/phip-web/src/main/java/com/zoe/phip/web/controller/Register/PersonnelController.java
+++ zoe-phip/4.ui/phip-web/src/main/java/com/zoe/phip/web/controller/Register/PersonnelController.java
@@ -1,8 +1,21 @@
 package com.zoe.phip.web.controller.Register;
 
+import com.zoe.phip.infrastructure.annotation.AuthAction;
+import com.zoe.phip.infrastructure.entity.PageList;
+import com.zoe.phip.infrastructure.entity.ServiceResult;
+import com.zoe.phip.infrastructure.entity.ServiceResultT;
+import com.zoe.phip.infrastructure.security.Permission;
+import com.zoe.phip.register.model.XmanBaseInfo;
+import com.zoe.phip.register.model.XmanCard;
+import com.zoe.phip.web.context.ComSession;
+import com.zoe.phip.web.context.ServiceFactory;
+
+import com.zoe.phip.web.controller.BaseController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -11,7 +24,7 @@ import javax.servlet.http.HttpServletRequest;
  */
 @Controller
 @RequestMapping("/personnel")
-public class PersonnelController {
+public class PersonnelController extends BaseController {
     //region 个人信息管理
     @RequestMapping("/view/xmanbaselist")
     public String ToXmanBaseList(HttpServletRequest request, Model model) {
@@ -21,6 +34,99 @@ public class PersonnelController {
     @RequestMapping("/view/xmanbasedetail")
     public String ToXmanBaseDetail(HttpServletRequest request, Model model) {
         return "/Register/Personnel/xmanBaseDetail";
+    }
+
+    /**
+     * 根据条件查询病人列表
+     *
+     * @param keyWord
+     * @return
+     */
+    @RequestMapping(value = "/getXmanList")
+    @ResponseBody
+    @AuthAction(permission = {Permission.Query}, name = "查询")
+    public ServiceResultT<PageList<XmanBaseInfo>> getXmanList(String keyWord) {
+        return ServiceFactory.getPatientRegisterIn().patientRegistryListQuery(ComSession.getUserInfo(), keyWord, getQueryPage());
+    }
+
+    /**
+     * 个人基本信息查询
+     *
+     * @param patientId
+     * @return
+     */
+    @RequestMapping(value = "/getXmanInfo")
+    @ResponseBody
+    @AuthAction(permission = {Permission.Query}, name = "查询")
+    public ServiceResultT<XmanBaseInfo> getXmanInfo(String patientId) {
+        return ServiceFactory.getPatientRegisterIn().patientRegistryQuery(ComSession.getUserInfo(), patientId);
+    }
+
+    /**
+     * 个人信息新增
+     *
+     * @param xmanBaseInfo
+     * @param xmanCard
+     * @return
+     */
+    @RequestMapping(value = "/addXmanInfo", method = RequestMethod.POST)
+    @ResponseBody
+    @AuthAction(permission = {Permission.Add}, name = "新增")
+    public ServiceResultT<XmanBaseInfo> addXmanInfo(XmanBaseInfo xmanBaseInfo, XmanCard xmanCard) {
+        return ServiceFactory.getPatientRegisterIn().addPatientRegistry(ComSession.getUserInfo(), xmanBaseInfo, xmanCard);
+    }
+
+    /**
+     * 个人信息更新
+     *
+     * @param xmanBaseInfo
+     * @param xmanCard
+     * @return
+     */
+    @RequestMapping(value = "/updateXmanInfo")
+    @ResponseBody
+    @AuthAction(permission = {Permission.Update}, name = "更新")
+    public ServiceResultT<XmanBaseInfo> updateXmanInfo(XmanBaseInfo xmanBaseInfo, XmanCard xmanCard) {
+        return ServiceFactory.getPatientRegisterIn().updatePatientRegistry(ComSession.getUserInfo(), xmanBaseInfo, xmanCard);
+    }
+
+    /**
+     * 个人身份合并
+     *
+     * @param newPatientId
+     * @param oldPatientId
+     * @return
+     */
+    @RequestMapping(value = "/mergeXmanInfo")
+    @ResponseBody
+    public ServiceResultT<XmanBaseInfo> mergeXManInfo(String newPatientId, String oldPatientId) {
+        return ServiceFactory.getPatientRegisterIn().mergePatientRegistry(ComSession.getUserInfo(), newPatientId, oldPatientId);
+    }
+
+    /**
+     * 批量删除个人信息
+     *
+     * @param ids
+     * @return
+     */
+    @RequestMapping(value = "/delXmanList")
+    @ResponseBody
+    @AuthAction(permission = {Permission.Delete}, name = "删除")
+    public ServiceResult delXmanList(String ids) {
+        return ServiceFactory.getPatientRegisterIn().deleteByIds(ComSession.getUserInfo(), ids);
+    }
+
+    /**
+     * 删除个人信息
+     *
+     * @param id
+     * @return
+     */
+    @RequestMapping(value = "/delXmanInfo")
+    @ResponseBody
+    @AuthAction(permission = {Permission.Delete}, name = "删除")
+    public ServiceResult delManInfo(String id) {
+        return ServiceFactory.getPatientRegisterIn().deleteById(ComSession.getUserInfo(), id);
     }
     //endregion
 
--- zoe-phip/4.ui/phip-web/src/main/webapp/assets/src/script/Register/Personnel/xmanBaseDetail.js
+++ zoe-phip/4.ui/phip-web/src/main/webapp/assets/src/script/Register/Personnel/xmanBaseDetail.js
@@ -8,9 +8,9 @@ define(function (require, exports, module) {
             var baseAttr = new BaseAttr({
                 winName: "win_xmanbase_detail_dialog",//弹窗对象变量名称
                 winCallback: "win_xmanbase_detail_callback",//弹窗回调函数
-                getUrl: '',//
-                addUrl: '',//新增接口Url
-                updateUrl: '',//修改接口Url
+                getUrl: 'personnel/getXmanInfo',//
+                addUrl: 'personnel/addXmanInfo',//新增接口Url
+                updateUrl: 'personnel/updateXmanInfo',//修改接口Url
                 loadPageEvent: function () {
                 }
             })
