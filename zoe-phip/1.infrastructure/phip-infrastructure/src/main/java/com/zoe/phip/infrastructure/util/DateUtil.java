05bdf9536396c043991d52a40d37a842759e49ae
Eason <zhangwenbin@outlook.com>
2016-04-29 17:31:41 +0800

    注册提示

--- zoe-phip/1.infrastructure/phip-infrastructure/src/main/java/com/zoe/phip/infrastructure/util/DateUtil.java
+++ zoe-phip/1.infrastructure/phip-infrastructure/src/main/java/com/zoe/phip/infrastructure/util/DateUtil.java
@@ -8,27 +8,30 @@ import java.util.Date;
  */
 public final class DateUtil {
 
-    public static Date stringToDateTime(String strDateTime) throws Exception {
-        SimpleDateFormat sdf = null;
-        if (strDateTime.length() == 8) {
-            sdf = new SimpleDateFormat("yyyy-MM-dd");
-            String reg = "(\\d{4})(\\d{2})(\\d{2})";
-            return sdf.parse(strDateTime.replaceAll(reg, "$1-$2-$3"));
-        } else if (strDateTime.length() == 12) {
-            sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-            String reg = "(\\d{4})(\\d{2})(\\d{2})(\\d{2})(\\d{2})";
-            return sdf.parse(strDateTime.replaceAll(reg, "$1-$2-$3 $4:$5"));
-        } else if (strDateTime.length() == 14) {
-            sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            String reg = "(\\d{4})(\\d{2})(\\d{2})(\\d{2})(\\d{2})(\\d{2})";
-            return sdf.parse(strDateTime.replaceAll(reg, "$1-$2-$3 $4:$5:$6"));
+    public static Date stringToDateTime(String strDateTime) {
+        try {
+            SimpleDateFormat sdf = null;
+            if (strDateTime.length() == 8) {
+                sdf = new SimpleDateFormat("yyyy-MM-dd");
+                String reg = "(\\d{4})(\\d{2})(\\d{2})";
+                return sdf.parse(strDateTime.replaceAll(reg, "$1-$2-$3"));
+            } else if (strDateTime.length() == 12) {
+                sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+                String reg = "(\\d{4})(\\d{2})(\\d{2})(\\d{2})(\\d{2})";
+                return sdf.parse(strDateTime.replaceAll(reg, "$1-$2-$3 $4:$5"));
+            } else if (strDateTime.length() == 14) {
+                sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                String reg = "(\\d{4})(\\d{2})(\\d{2})(\\d{2})(\\d{2})(\\d{2})";
+                return sdf.parse(strDateTime.replaceAll(reg, "$1-$2-$3 $4:$5:$6"));
+            }
+            if (strDateTime.contains("T")) {
+                sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ssz");
+                String reg = "(\\d{4})(\\d{2})(\\d{2})(\\d{2})(\\d{2})(\\d{2})";
+                return sdf.parse(strDateTime.replaceAll(reg, "$1-$2-$3 $4:$5:$6"));
+            }
+            return new Date();
+        } catch (Exception ex) {
+            return new Date();
         }
-        if (strDateTime.contains("T")) {
-            sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ssz");
-            String reg = "(\\d{4})(\\d{2})(\\d{2})(\\d{2})(\\d{2})(\\d{2})";
-            return sdf.parse(strDateTime.replaceAll(reg, "$1-$2-$3 $4:$5:$6"));
-        }
-
-        return new Date();
     }
 }
