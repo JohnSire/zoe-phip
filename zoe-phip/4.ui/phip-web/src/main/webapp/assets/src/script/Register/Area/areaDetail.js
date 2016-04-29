ae867763f2c6881abb69b51e0ea6332090fadffd
zxc <456>
2016-04-29 10:00:28 +0800

    行政区划父级区域弹窗

--- zoe-phip/4.ui/phip-web/src/main/webapp/assets/src/script/Register/Area/areaDetail.js
+++ zoe-phip/4.ui/phip-web/src/main/webapp/assets/src/script/Register/Area/areaDetail.js
@@ -3,6 +3,7 @@
  */
 define(function (require, exports, module) {
     var internal = {
+        selectList: require("{dir}/UtilityModule/SelectList/list"),
         init: function () {
             var BaseAttr = require("{staticDir}/BaseAttr/baseAttr");
             var baseAttr = new BaseAttr({
@@ -12,6 +13,27 @@ define(function (require, exports, module) {
                 addUrl: 'area/addAreaInfo',//新增接口Url
                 updateUrl: 'area/updateAreaInfo',//修改接口Url
                 loadPageEvent: function () {
+                    $(".btn-switch-outer").btnSwitch({name: 'state'});
+                    internal.selectList.dialog('area', {
+                        target: $("#btnArea"),
+                        name: 'pid',
+                        parentName: 'parentName',
+                        valueField: 'id',
+                        displayField: 'name',
+                        fkNullContent: '根级节点',
+                        selectParam: {
+                            isTreeVaild: true,//如果是树节点，父节点不能是其本身验证
+                            treeVaildMsg: '父级分类不能是其本身!',
+                            multiselect: false
+                        },
+                        buttonsExtend: [{
+                            text: '根级节点', onclick: function (item, dialog) {
+                                $('input[name="pid"]').val(0);
+                                $("#btnArea").find(".text-line-content").text("根级节点");
+                                dialog.close();
+                            }
+                        }]
+                    });
                 }
             })
         }
