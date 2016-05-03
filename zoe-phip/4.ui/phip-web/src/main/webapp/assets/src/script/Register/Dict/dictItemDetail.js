79331c4f30556f3768fed5b909da912e24466e56
qinghuangl <814400623@qq.com>
2016-05-03 15:52:38 +0800

    新增字典项

---
+++ zoe-phip/4.ui/phip-web/src/main/webapp/assets/src/script/Register/Dict/dictItemDetail.js
@@ -0,0 +1,13 @@
+/**
+ * Created by linqinghuang on 2016/5/3.
+ */
+define(function (require, exports, module) {
+    var internal = {
+        init: function () {
+        }
+    };
+    exports.init = function () {
+        internal.init();
+    }
+
+})
