d7ca5e108b037d9570200b15e26e8e986b29f890
hyf <510287342@qq.com>
2016-05-05 11:00:13 +0800

    去掉测试代码

--- zoe-phip/2.module/phip-module-service-impl/src/main/java/com/zoe/phip/module/service/support/dubbo/DynamicProxyInvoker.java
+++ zoe-phip/2.module/phip-module-service-impl/src/main/java/com/zoe/phip/module/service/support/dubbo/DynamicProxyInvoker.java
@@ -9,11 +9,8 @@ import com.zoe.phip.infrastructure.entity.SystemData;
 import com.zoe.phip.infrastructure.function.Function;
 import com.zoe.phip.infrastructure.security.SystemCredential;
 import com.zoe.phip.infrastructure.util.SafeExecuteUtil;
-import com.zoe.phip.module.service.entity.BaseEntity;
 import com.zoe.phip.module.service.impl.in.IBaseInService;
 import com.zoe.phip.module.service.support.annotation.WithResult;
-import com.zoe.phip.module.service.validator.ValidationAppendUtils;
-import com.zoe.phip.module.service.validator.ValidationResult;
 
 import java.lang.reflect.Method;
 import java.util.ArrayList;
@@ -115,21 +112,6 @@ public class DynamicProxyInvoker<T> extends AbstractProxyInvoker<T> {
                 ServiceResult result = new ServiceResult();
                 result.addMessage(ErrorCode.DEFAULT, "方法的第一个参数必须为SystemData类型");
                 return result;
-            }
-        }
-
-
-        if (methodName.startsWith("add")) {
-            for (Object o : arguments) {
-                if(o instanceof BaseEntity){
-                    ValidationResult result = ValidationAppendUtils.validateEntity(o);
-                    if(result.isHasErrors()){
-                        ServiceResult serviceResult=new ServiceResult();
-                        serviceResult.addMessage(ErrorCode.VALIDATOR_ERROR,result.getErrorMessage());
-                        return serviceResult;
-                    }
-
-                }
             }
         }
 
