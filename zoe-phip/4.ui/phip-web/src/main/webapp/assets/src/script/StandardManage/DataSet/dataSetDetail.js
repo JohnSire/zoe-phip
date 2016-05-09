3961dd9e1c9c1a94d2bdaa202211ae4861f1772a
chenzhisen <292963218@qq.com>
2016-05-09 11:58:20 +0800

    数据集 CDA页面

--- zoe-phip/4.ui/phip-web/src/main/webapp/assets/src/script/StandardManage/DataSet/dataSetDetail.js
+++ zoe-phip/4.ui/phip-web/src/main/webapp/assets/src/script/StandardManage/DataSet/dataSetDetail.js
@@ -1,3 +1,85 @@
 /**
  * Created by chenzhisen on 2016/5/6.
  */
+define(function (require, exports, module) {
+    var internal = {
+        selectList: require("{dir}/UtilityModule/SelectList/list"),
+        init: function () {
+            var BaseAttr = require("{staticDir}/BaseAttr/baseAttr");
+            var baseAttr = new BaseAttr({
+                winName: "win_dictCategory_detail_dialog",//弹窗对象变量名称
+                winCallback: "win_dictCategory_detail_callback",//弹窗回调函数
+                getUrl: 'dict/getDictCatalogInfoById',//
+                addUrl: 'dict/addDictCatalogInfo',//新增接口Url
+                updateUrl: 'dict/updateDictCatalogInfo',//修改接口Url
+                loadPageEvent: function () {
+                    internal.selectList.dialog('dataSetColumn', {
+                        target: $("#btnBusinessColumn"),
+                        name: 'pid',
+                        parentName: 'parentName',
+                        valueField: 'id',
+                        displayField: 'name',
+                        fkNullContent: '根级节点',
+                        selectParam: {
+                            isTreeVaild: true,//如果是树节点，父节点不能是其本身验证
+                            treeVaildMsg: '父级分类不能是其本身!',
+                            multiselect: false
+                        },
+                        buttonsExtend: [{
+                            text: '根级节点', onclick: function (item, dialog) {
+                                $('input[name="pid"]').val(0);
+                                $("#btnParentCatalog").find(".text-line-content").text("根级节点");
+                                dialog.close();
+                            }
+                        }]
+                    });
+                    internal.selectList.dialog('dataSetColumn', {
+                        target: $("#btnDataUpdateColumn"),
+                        name: 'pid',
+                        parentName: 'parentName',
+                        valueField: 'id',
+                        displayField: 'name',
+                        fkNullContent: '根级节点',
+                        selectParam: {
+                            isTreeVaild: true,//如果是树节点，父节点不能是其本身验证
+                            treeVaildMsg: '父级分类不能是其本身!',
+                            multiselect: false
+                        },
+                        buttonsExtend: [{
+                            text: '根级节点', onclick: function (item, dialog) {
+                                $('input[name="pid"]').val(0);
+                                $("#btnParentCatalog").find(".text-line-content").text("根级节点");
+                                dialog.close();
+                            }
+                        }]
+                    });
+
+                    internal.selectList.dialog('dataSet', {
+                        target: $("#btnDataSet"),
+                        name: 'pid',
+                        parentName: 'parentName',
+                        valueField: 'id',
+                        displayField: 'name',
+                        fkNullContent: '根级节点',
+                        selectParam: {
+                            isTreeVaild: true,//如果是树节点，父节点不能是其本身验证
+                            treeVaildMsg: '父级分类不能是其本身!',
+                            multiselect: false
+                        },
+                        buttonsExtend: [{
+                            text: '根级节点', onclick: function (item, dialog) {
+                                $('input[name="pid"]').val(0);
+                                $("#btnParentCatalog").find(".text-line-content").text("根级节点");
+                                dialog.close();
+                            }
+                        }]
+                    });
+
+                }
+            })
+        }
+    }
+    exports.init = function () {
+        internal.init();
+    }
+});
