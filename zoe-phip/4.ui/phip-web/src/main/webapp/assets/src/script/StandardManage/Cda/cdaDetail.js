1b9c77a293d08547aea4f963ae66577eb9362d4a
zxc <456>
2016-05-10 17:17:18 +0800

    Cda接口对接

--- zoe-phip/4.ui/phip-web/src/main/webapp/assets/src/script/StandardManage/Cda/cdaDetail.js
+++ zoe-phip/4.ui/phip-web/src/main/webapp/assets/src/script/StandardManage/Cda/cdaDetail.js
@@ -1,3 +1,24 @@
 /**
  * Created by chenzhisen on 2016/5/6.
  */
+define(function (require, exports, module) {
+    var internal = {
+        init: function () {
+            var BaseAttr = require("{staticDir}/BaseAttr/baseAttr");
+            var baseAttr = new BaseAttr({
+                    winName: "win_cda_detail_dialog",//弹窗对象变量名称
+                    winCallback: "win_cda_detail_callback",//弹窗回调函数
+                    getUrl: 'cda/getCdaInfo',//
+                    addUrl: 'cda/addCdaInfo',//新增接口Url
+                    updateUrl: 'cda/updateCdaInfo',//修改接口Url
+                    loadPageEvent: function () {
+                    }
+                }
+            );
+        },
+    };
+    exports.init = function () {
+        internal.init();
+    }
+})
+;
--- zoe-phip/4.ui/phip-web/src/main/webapp/assets/src/script/StandardManage/Cda/cdaList.js
+++ zoe-phip/4.ui/phip-web/src/main/webapp/assets/src/script/StandardManage/Cda/cdaList.js
@@ -5,14 +5,14 @@
 define(function (require, exports, module) {
     var internal = {
         init: function () {
-            window.toDataSetList=toDataSetList;
-            window.xsl=xsl.show;
+            window.toDataSetList = toDataSetList;
+            window.xsl = xsl.show;
             var BaseGrid = require("{staticDir}/BaseGrid/baseGrid");
             var baseGrid = new BaseGrid({
                 gridId: 'grid',
                 deleteUrl: {
-                    deleteInfo: "personnel/delXmanInfo",
-                    deleteList: "personnel/delXmanList"
+                    deleteInfo: "cda/delCdaInfo",
+                    deleteList: "cda/delCdaList"
                 },
                 tools: {
                     btnbox: {
@@ -25,26 +25,32 @@ define(function (require, exports, module) {
                 },
                 // reqInfoKey:'patientId',
                 gridParam: {
-                    url: 'personnel/getXmanList',
+                    url: 'cda/getCdaList',
                     columns: [
-                        {display: '标识符', name: 'cardCode', width: 120, align: 'left'},
+                        {display: '标识符', name: 'code', width: 120, align: 'left'},
                         {display: '名称', name: 'name', width: 120, align: 'left'},
-                        {display: '档案类别', name: 'sexCodeName', width: 120, align: 'left'},
-                        {display: '描述', name: 'idNo', width: 120, align: 'left'},
-                        {display: '关联数据集',  width: 120,render:function(rowdata, rowindex, value){
+                        {display: '档案类别', name: 'archiveName', width: 120, align: 'left'},
+                        {display: '描述', name: 'descr', width: 120, align: 'left'},
+                        {
+                            display: '关联数据集', width: 120, render: function (rowdata, rowindex, value) {
                             var h = "";
                             h += "<a class='icon-grid icon-grid-setting' title='配置'"
-                            + " onclick='javascript:toDataSetList(\"" + rowdata.id + "\",\"" + rowdata.name + "\")'></a>";;
+                                + " onclick='javascript:toDataSetList(\"" + rowdata.id + "\",\"" + rowdata.name + "\")'></a>";
+                            ;
 
                             return h;
-                        }},
-                        {display: 'XSL配置',  width: 120,render:function(rowdata, rowindex, value){
+                        }
+                        },
+                        {
+                            display: 'XSL配置', width: 120, render: function (rowdata, rowindex, value) {
                             var h = "";
                             h += "<a class='icon-grid icon-grid-setting' title='配置'"
-                            + " onclick='javascript:xsl(\"" + rowdata.id + "\",\"" + rowdata.name + "\")'></a>";;
+                                + " onclick='javascript:xsl(\"" + rowdata.id + "\",\"" + rowdata.name + "\")'></a>";
+                            ;
 
                             return h;
-                        }},
+                        }
+                        },
                         {display: '操作', isSort: false, width: 120, icons: ['edit', 'del']}
                     ],
                     usePage: true,
@@ -52,8 +58,8 @@ define(function (require, exports, module) {
                     height: $("body").innerHeight() - $("#gridTools").outerHeight() - 38//500
                 },
                 dialogParam: {
-                    winName: "win_xmanbase_detail_dialog",//弹窗对象变量名称
-                    winCallback: "win_xmanbase_detail_callback",//弹窗回调函数
+                    winName: "win_cda_detail_dialog",//弹窗对象变量名称
+                    winCallback: "win_cda_detail_callback",//弹窗回调函数
                     titleKey: "name",
                     //新增参数
                     add: {title: "新增Cda"},
@@ -61,8 +67,8 @@ define(function (require, exports, module) {
                     edit: {title: "编辑Cda"},
                     common: {
                         url: 'cda/view/cdaDetail',
-                        width: 680,
-                        height: 580
+                        width: 450,
+                        height: 320
                     }
                 }
             })
@@ -70,43 +76,46 @@ define(function (require, exports, module) {
 
 
     };
-    var xsl={
-        show:function(){
-          var dialogParam=
-          {
-              title: "xsl编辑",
+
+    exports.init = function () {
+        internal.init();
+    }
+
+
+    var xsl = {
+        show: function () {
+            var dialogParam =
+            {
+                title: "xsl编辑",
                 url: 'cda/view/editXsl',
-                    width: 680,
-                    height: 480,
-              buttons: [
-                  {
-                      //verifySubmit自己的验证提交状态
-                      text: "确定",
-                      verifySubmit: true,
-                      submitText: '提交中...',
-                      onclick: function (item, dialog, submited) {
-                      }
-                  },
-                  {
-                      text: "取消",
-                      onclick: function (item, dialog) {
-                          dialog.close();
-                      }
-                  }
-              ]
+                width: 680,
+                height: 480,
+                buttons: [
+                    {
+                        //verifySubmit自己的验证提交状态
+                        text: "确定",
+                        verifySubmit: true,
+                        submitText: '提交中...',
+                        onclick: function (item, dialog, submited) {
+                        }
+                    },
+                    {
+                        text: "取消",
+                        onclick: function (item, dialog) {
+                            dialog.close();
+                        }
+                    }
+                ]
             }
             var top = common.getTopWindowDom();
             top["show"] = common.dialog(dialogParam);
         }
 
     }
-function toDataSetList(id,name){
-    var top = common.getTopWindowDom();
-    var link=webRoot+"cda/view/dataSetList?cdaName="+name;
-    top. frames["mainframe"].location.href = link;
-}
-    exports.init = function () {
-        internal.init();
-    }
 
+    function toDataSetList(id, name) {
+        var top = common.getTopWindowDom();
+        var link = webRoot + "cda/view/dataSetList?cdaName=" + name;
+        top.frames["mainframe"].location.href = link;
+    }
 });
